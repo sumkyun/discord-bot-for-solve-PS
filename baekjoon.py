3a3f4e63b6ca734d5e7dc00ddc79a9d8ac5dada5
kkhmsg30 <kkhmsg30@naver.com>
2022-08-10 09:28:49 +0900

    문제 출제

--- baekjoon.py
+++ baekjoon.py
@@ -1,8 +1,11 @@
 import http.client
-import random
 import json
 import discord
 import datetime
+
+bot_log=1006707877400031283 # 앵글리에
+# bot_log=1006710158417727633 # 내거
+
 
 def get_profile_by_id(id):
     conn = http.client.HTTPSConnection("solved.ac")
@@ -18,7 +21,7 @@ def get_problems():
     conn.request("GET", r"/api/v3/search/problem?query=*s2..p5%20s%23100..%20lang%3Ako%20solvable%3Atrue%20!s%40kkhmsg30%20!s%40tndyd0706%20!s%40dandelion51&page=1&sort=random", headers=headers)
     res = conn.getresponse()
     data = res.read()
-    json.loads(data.decode("utf-8"))
+    return json.loads(data.decode("utf-8"))
 
 def is_solved(problem, id):
     conn = http.client.HTTPSConnection("solved.ac")
@@ -33,29 +36,34 @@ async def on_message(message):
     args=message.content.split()[1:] # 나머지 단어 리스트
     if key=='경민아':
         try:
-            if len(args)==0: # 나머지 단어가 비어있을 때
+            # 비어있을때
+            if len(args)==0:
                 await message.reply('흥! 어쩌라구요!')
             
+            # 세 문제 정하기
             if len(args)==1 and args[0]=='문제줘':
-                data=get_problems()
-                Max=2*max(len(data['items'][i]['titleKo']) for i in range(3))
-                emptystr=[' '*(Max-len(data['items'][i]['titleKo'])) for i in range(3)]
-                embed=discord.Embed(title='오늘의 문제', description=f"""**{data['items'][0]['titleKo']}** - [{data['items'][0]['problemId']}](https://www.acmicpc.net/problem/{data['items'][0]['problemId']})
-                **{data['items'][1]['titleKo']}** - [{data['items'][1]['problemId']}](https://www.acmicpc.net/problem/{data['items'][1]['problemId']})
-                **{data['items'][2]['titleKo']}** - [{data['items'][2]['problemId']}](https://www.acmicpc.net/problem/{data['items'][2]['problemId']})""")
-                # embed.add_field(name=data['items'][0]['titleKo'], value=str(data['items'][0]['problemId']),inline=True)
-                # embed.add_field(name=data['items'][1]['titleKo'], value=str(data['items'][1]['problemId']),inline=True)
-                # embed.add_field(name=data['items'][2]['titleKo'], value=str(data['items'][2]['problemId']),inline=True)
-                
-                await message.reply(embed=embed)
+                today=datetime.datetime.today().strftime(r"%Y%m%d")
+                messages=[m async for m in message.guild.get_channel(bot_log).history(limit=1)]
+                if messages[0].content==today:
+                    await message.reply('아까 알려줬자나유!', embed=messages[0].embeds[0])
+                else:
 
+                    data=get_problems()
+                    embed=discord.Embed(title='오늘의 문제', description=f"""**{data['items'][0]['titleKo']}** - [{data['items'][0]['problemId']}](https://www.acmicpc.net/problem/{data['items'][0]['problemId']})
+                    **{data['items'][1]['titleKo']}** - [{data['items'][1]['problemId']}](https://www.acmicpc.net/problem/{data['items'][1]['problemId']})
+                    **{data['items'][2]['titleKo']}** - [{data['items'][2]['problemId']}](https://www.acmicpc.net/problem/{data['items'][2]['problemId']})""")
+
+                    await message.guild.get_channel(bot_log).send(today, embed=embed)
+                    await message.reply(embed=embed)
+            
+            # 풀었는지 판별하기
             if len(args)==3 and (args[2]=='풀었어'):
                 if is_solved(args[0], args[1]) or is_solved(args[1], args[0]):
                     await message.reply('진짜넹')
                 else:
                     await message.reply('거짓말 치지 마유!')
 
-
+            # 백준 프로필 정보
             if len(args)==2 and (args[1]=='티어' or args[1]=='정보' or args[1]=='프로필'):
                 data=get_profile_by_id(args[0])
 
@@ -66,5 +74,6 @@ async def on_message(message):
                 embed.set_thumbnail(url=f"attachment://{data['tier']}.png")
                 
                 await message.reply(embed=embed, file=image)
+
         except Exception as e:
             await message.reply('흥! 코드 제대로 짜세유!\nError:'+str(e))
